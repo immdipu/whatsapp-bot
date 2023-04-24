a5f402110b42f3f9924696089e8f549ccd098360
Dipu <dipuchaurasiya91@gmail.com>
2023-04-24 14:07:35 +0545

    get req added

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ app.get("/webhooks", (req, res) => {
   let mode = req.query["hub.mode"];
   let challenge = req.query["hub.challenge"];
   let token = req.query["hub.verify_token"];
-  console.log(req);
+  if (mode === "subscribe" && token === mytoken) {
+    res.status(200).send(challenge);
+  } else {
+    res.status(403);
+  }
 });
