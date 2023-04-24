99133a26cad26e86bcde1718298efcbb84af8fdb
Dipu <dipuchaurasiya91@gmail.com>
2023-04-24 13:58:48 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
+dotenv.config();
+
+let port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log("Server started sucessfully");
+});
+
+app.get("/webhooks", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let challenge = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
+  console.log(req);
+});
